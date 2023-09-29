53439c8896d756740832daa05c0104952d99950b
Karly Berrning <berningkarly@Karlys-MacBook-Air.local>
2023-09-28 19:34:42 -0500

    Candidate Testing Final Final part

--- candidate-testing.js
+++ candidate-testing.js
@@ -67,8 +67,6 @@ function runProgram() {
   askQuestion();
   gradeQuiz(candidateAnswers);
 }
-runProgram()
-
 // ----------- Don't write any code or change any code below this line ---------- //
 module.exports = {
   candidateName: candidateName,
